452f28fce4b44f11ff71f8d3af84d4683a8c9fce
Chinomiy <maruishiwo@gmail.com>
2023-10-12 18:27:53 +0300

    v4

--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -3,12 +3,17 @@ package school.mjc.stage0.module3.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-         variable = 2000;
-        int value = 1000;
-         variable = 4000;
-        int valueTwo = 3000;
+        int variableTwo = 2000;
+        int valueOfSomething = 1000;
+        int variableThree = 4000;
+        int valueFour = 3000;
         int variables = 5000;
         variables = 7000;
-         value = 90000;
+        int value = 90000;
+        int valueOfIntegerNumbers = 100000;
+        int ownVariable = 1000000;
+        int counterVariable = 1234567;
+        int valueOfCurrentSum = 1234567;
     }
 }
+
