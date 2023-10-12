dc6da112df19a2aac2f932e8ceb5dcd65f153a4e
Chinomiy <maruishiwo@gmail.com>
2023-10-12 18:22:52 +0300

    v23

--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -6,7 +6,7 @@ public class CopiedNames {
          variable = 2000;
         int value = 1000;
          variable = 4000;
-        int value2 = 3000;
+        int valueTwo = 3000;
         int variables = 5000;
         variables = 7000;
          value = 90000;
